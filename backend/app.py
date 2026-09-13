62f5777d9028bdf67e5cfdb435fa73a12dc547a5
Aasish B Vijayan <aasishvijayan@gmail.com>
2026-09-13 18:03:23 +0530

    Update .gitignore, restructure app.py for dotenv integration, and add requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 from flask import Flask, render_template, request, jsonify
 from gemini_client import GeminiClient
 
-app = Flask(__name__, template_folder='../templates')
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR/".env")
+
+app = Flask(__name__, template_folder=str(BASE_DIR.parent / 'templates'))
 client = GeminiClient
 
 @app.route("/")
